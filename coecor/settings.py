0fd4fa7c2d1b1b706a7f44b59e74eccf71f011fb
evnsan <evandro@cobli.co>
2018-01-04 00:31:09 -0200

    Add django-bootstrap4

--- coecor/settings.py
+++ coecor/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'imoveis.apps.ImoveisConfig',
     'db_file_storage',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
