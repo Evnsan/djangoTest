755b8d8b47d7ce96177eb626dc11519a6e7b8f74
evnsan <evandro@cobli.co>
2018-01-05 00:13:43 -0200

    imoveis: Fix imoveis strict urls ie. ^$ -> ^

--- imoveis/urls.py
+++ imoveis/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'imoveis'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<build_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<build_id>[0-9]+)/picture$', views.picture, name='picture'),
+    url(r'^(?P<build_id>[0-9]+)/', views.detail, name='detail'),
     url('add_picture', views.add_picture, name='add_picture'),
     url('add_observation', views.add_observation, name='add_observation'),
 ]
