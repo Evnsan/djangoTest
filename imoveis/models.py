4389954d336a8b8a857dfc54314803fb9ac29bd3
evnsan <evandro@cobli.co>
2018-01-26 17:52:03 -0200

    imoveis: Install django-money

--- imoveis/models.py
+++ imoveis/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import RegexValidator
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
 
