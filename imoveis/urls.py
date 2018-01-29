7ed68a3920669cb34746bc1b0b0d42ed7f597664
evnsan <evandro@cobli.co>
2018-01-28 23:43:45 -0200

    imoveis: Add workaround for infinity redirect problem

--- imoveis/urls.py
+++ imoveis/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 app_name = 'imoveis'
 urlpatterns = [
-    url(r'^$', views.BuildList.as_view(), name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^(?P<build_id>[0-9]+)/picture$', views.picture, name='picture'),
     url(r'^(?P<pk>[0-9]+)/', views.BuildDetail.as_view(), name='detail'),
     url(r'search', views.search, name='search'),
